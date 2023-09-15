db1c30c3ff1083c13c059e4873254260d37ea032
Ben <baguirre@luc.edu>
2023-09-15 11:49:15 -0500

    working drop-down

--- src/dropDown.jsx
+++ src/dropDown.jsx
@@ -29,7 +29,14 @@ export default function Dropdown({
       <div className="top">
         <h3>{section}</h3>
         <button onClick={handleDrop} className="collapse">
-          Arrow-two
+          <svg
+            className="arrow-up"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <title>arrow-down-drop-circle</title>
+            <path d="M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M7,10L12,15L17,10H7Z" />
+          </svg>
         </button>
       </div>
       <div className="form-display">
@@ -40,7 +47,16 @@ export default function Dropdown({
   ) : (
     <div className="drop-down">
       <h3>{section}</h3>
-      <button onClick={handleDrop}>Down Arrow</button>
+      <button className="collapse" onClick={handleDrop}>
+        <svg
+          className="drop-down-svg"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <title>arrow-down-drop-circle</title>
+          <path d="M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M7,10L12,15L17,10H7Z" />
+        </svg>
+      </button>
     </div>
   );
 }
